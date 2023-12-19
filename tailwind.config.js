f92cd1b9df72280f13ad0402e78d51e9fb60b3c1
pnasiba <pnasiba23.06@gmail.com>
2023-12-20 02:41:22 +0500

    Ashyo

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html" , "./pages/products.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+
